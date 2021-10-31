5e79e5e39f876476634d24f5f58692b68ccf97fc
Majeed <rahmanmajeed46@gmail.com>
2021-11-01 00:12:16 +0600

    [refactor] all shared components.

--- src/components/shared/Header.js
+++ src/components/shared/Header.js
@@ -1,7 +1,7 @@
 import { useState } from "react/cjs/react.development";
 import Icon from "../icons/Icon";
 
-function Header({ handleSidebar }) {
+function Header({ sidebar, handleSidebar }) {
   const [mobileSidebar, setMobileSidebar] = useState(false);
 
   const handleHeader = () => {
@@ -21,15 +21,19 @@ function Header({ handleSidebar }) {
                 : "hidden"
             }`}
           >
-            <button
-              type="button"
-              className="text-yellow-500"
-              onClick={handleMobileHeader}
-            >
-              <Icon icon="#menu" size="4" />
-            </button>
+            {/** mobile sidebar top */}
+            <div className="flex justify-between items-center px-5 py-3">
+              <div>
+                <Icon icon="#site_logo" size="10" />
+              </div>
+              <div>
+                <button onClick={handleMobileHeader}>
+                  <Icon icon="#open" size="4" />
+                </button>
+              </div>
+            </div>
           </div>
-          <div className="flex flex-1 px-5 py-3 items-center justify-between bg-gray-300">
+          <div className="flex flex-1 px-5 py-2 items-center justify-between bg-gray-300">
             <div className="flex flex-1">
               <div className="hidden md:block px-2">
                 <button
@@ -37,7 +41,11 @@ function Header({ handleSidebar }) {
                   className="text-pink-800"
                   onClick={handleHeader}
                 >
-                  <Icon icon="#menu" size="4" />
+                  {sidebar ? (
+                    <Icon icon="#open" size="4" />
+                  ) : (
+                    <Icon icon="#close" size="4" />
+                  )}
                 </button>
               </div>
               <div className="block md:hidden px-2">
@@ -46,13 +54,29 @@ function Header({ handleSidebar }) {
                   className="text-yellow-500"
                   onClick={handleMobileHeader}
                 >
-                  <Icon icon="#menu" size="4" />
+                  <Icon icon="#close" size="4" />
                 </button>
               </div>
-              <div className="px-2">Logo</div>
+              <div className="px-2 md:hidden">
+                <Icon icon="#site_logo_dark" size="5" />
+              </div>
             </div>
-            <div className="flex">
-              <div className="w-8 h-8 rounded-full bg-gray-400"></div>
+            {/** right side header */}
+            <div className="flex justify-between items-center">
+              <div className="px-3">
+                <button type="button" className="mr-6 py-1">
+                  <Icon icon="#bell" size="5" />
+                </button>
+              </div>
+              <div className="w-8 h-8 mr-5 rounded-full bg-white">
+                <button type="button">
+                  <img
+                    src={process.env.PUBLIC_URL + "/user.png"}
+                    alt="img"
+                    className="h-full w=full object-cover rounded-full"
+                  />
+                </button>
+              </div>
             </div>
           </div>
         </header>
--- src/components/shared/Sidebar.js
+++ src/components/shared/Sidebar.js
@@ -1,7 +1,12 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 import routes from "../../routes";
+import Icon from "../icons/Icon";
 
-function Sidebar({ sidebar }) {
+function Sidebar({ sidebar, handleSidebar }) {
+  const currentLocation = useLocation();
+  const activeClass = "bg-gray-light text-white rounded-2xl py-2";
+  const deActiveClass = "py-3 text-gray-200";
+
   return (
     <>
       <div
@@ -11,11 +16,43 @@ function Sidebar({ sidebar }) {
             : "hidden"
         }`}
       >
-        {routes.map((item, index) => (
-          <div key={index} className="text-green-50">
-            <NavLink to={item.path}>{item.name}</NavLink>
+        {/** sidebar top */}
+        <div className="flex justify-between items-center px-5 py-3">
+          <div>
+            <Icon icon="#site_logo" size="10" />
           </div>
-        ))}
+          <div>
+            <button onClick={() => handleSidebar(false)}>
+              <Icon icon="#open" size="4" />
+            </button>
+          </div>
+        </div>
+
+        {/** sidebar items */}
+        <div className="m-3 pt-5">
+          {routes.map((item, index) => (
+            <div key={index}>
+              <NavLink
+                to={item.path}
+                className="ml-12px"
+                activeStyle={{ color: "black" }}
+              >
+                <div
+                  className={`flex gap-2 lg:gap-3 justify-start items-center ${
+                    item.path === currentLocation.pathname
+                      ? activeClass
+                      : deActiveClass
+                  }`}
+                >
+                  <div className="pl-5">
+                    <Icon icon={item.icon} size="4" />
+                  </div>
+                  <div className="">{item.name}</div>
+                </div>
+              </NavLink>
+            </div>
+          ))}
+        </div>
       </div>
     </>
   );
